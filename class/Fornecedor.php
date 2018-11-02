d340fd51afd57b6cb271eda00437a289b6f07ec8
Dari Osorio Junior <juniorosorio47@gmail.com>
2018-11-01 22:31:19 -0300

    Getters and setters da classe Fornecedores

--- class/Fornecedor.php
+++ class/Fornecedor.php
@@ -6,4 +6,44 @@ class Fornecedor{
     private $cnpj;
     private $endereco;
     private $contato;
+
+    public function setNomeFantasia($nome){
+        $this->nomeFantasia = $nome;
+    }
+
+    public function getNomeFantasia(){
+        return $this->nomeFantasia;
+    }
+
+    public function setrazaoSocial($nome){
+        $this->razaoSocial = $nome;
+    }
+
+    public function getRazaoSocial(){
+        return $this->razaoSocial;
+    }
+
+    public function setCnpj($cnpj){
+        $this->cnpj = $cnpj;
+    }
+
+    public function getCnpj(){
+        return $this->cnpj;
+    }
+
+    public function setEndereco($endereco){
+        $this->endereco = $endereco;
+    }
+
+    public function getEndereco(){
+        return $this->endereco;
+    }
+
+    public function setContato($contato){
+        $this->contato = $contato;
+    }
+
+    public function getContato(){
+        return $this->contato;
+    }
 }
